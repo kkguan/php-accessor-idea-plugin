51d0fb03b11e2d7517a0bed435f37e4375801c27
free2one <free2someone@outlook.com>
2023-03-10 17:57:55 +0800

    refactor: 更改文件订阅方式

--- src/main/java/com/free2one/accessor/composer/ComposerBulkFileListener.java
+++ src/main/java/com/free2one/accessor/composer/ComposerBulkFileListener.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectLocator;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.psi.PsiFile;
@@ -30,20 +31,32 @@ import java.util.List;
 
 public class ComposerBulkFileListener implements BulkFileListener {
 
-    private final Project project;
+    private final Project project = null;
 
-    public ComposerBulkFileListener(Project project) {
-        this.project = project;
+    public ComposerBulkFileListener() {
     }
+
+//    public ComposerBulkFileListener(Project project) {
+//        this.project = project;
+//    }
 
     @Override
     public void after(@NotNull List<? extends @NotNull VFileEvent> events) {
-        events.forEach(this::generateProxy);
+        events.forEach(this::generateProxy2);
     }
 
-    private void generateProxy(VFileEvent vFileEvent) {
+
+    private void generateProxy2(VFileEvent vFileEvent) {
+        if (vFileEvent.getFile() == null) {
+            return;
+        }
+        Project project = ProjectLocator.getInstance().guessProjectForFile(vFileEvent.getFile());
+        if (project == null) {
+            return;
+        }
+
         AccessorSettings settings = project.getService(AccessorSettings.class);
-        if (!isPendingFile(vFileEvent, settings)) {
+        if (!isPendingFile(vFileEvent, settings, project)) {
             return;
         }
 
@@ -71,7 +84,37 @@ public class ComposerBulkFileListener implements BulkFileListener {
         }
     }
 
-    private boolean isPendingFile(VFileEvent vFileEvent, AccessorSettings settings) {
+//    private void generateProxy(VFileEvent vFileEvent) {
+//        AccessorSettings settings = project.getService(AccessorSettings.class);
+//        if (!isPendingFile(vFileEvent, settings)) {
+//            return;
+//        }
+//
+//        ArrayList<String> commandLineOptions = new ArrayList<>();
+//        commandLineOptions.add(vFileEvent.getFile().getPath());
+//        commandLineOptions.add("-d");
+//        commandLineOptions.add(project.getBasePath());
+//        commandLineOptions.add("--");
+//        commandLineOptions.add("--dir=" + settings.getProxyRootDirectory());
+//        commandLineOptions.add("--gen-meta=yes");
+//        List<String> command = ComposerUtils.getRunScriptCommand("php-accessor", commandLineOptions);
+//        ComposerExecution composer = ComposerDataService.getInstance(project).getComposerExecution();
+//        Task.Backgroundable task = new Task.Backgroundable(project, AccessorBundle.message("composer.generate-proxy.task.title")) {
+//            @Override
+//            public void run(@NotNull ProgressIndicator indicator) {
+//                ComposerCommandRunner runner = new ComposerCommandRunner(composer, project, project.getBasePath(), new ComposerProgressIndicator());
+//                ComposerCommandRunner.ExecutionResult result = runner.runCommand(command, new ComposerProcessListener());
+//            }
+//        };
+//        if (SwingUtilities.isEventDispatchThread()) {
+//            ProgressManager.getInstance().run(task);
+//        } else {
+//            // Run the scan task when the thread is in the foreground.
+//            SwingUtilities.invokeLater(() -> ProgressManager.getInstance().run(task));
+//        }
+//    }
+
+    private boolean isPendingFile(VFileEvent vFileEvent, AccessorSettings settings, Project project) {
         if (vFileEvent.getFile() == null ||
                 !(vFileEvent.getFile().getFileType() instanceof PhpFileType) ||
                 vFileEvent.getPath().contains(project.getBasePath() + File.separator + "vendor")) {
